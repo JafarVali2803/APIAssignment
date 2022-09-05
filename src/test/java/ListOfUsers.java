02408564fc6c6411f79063afa1672c0144db104e
Jafar Vali Juturu <jafarj@testvagrant.com>
2022-09-05 11:47:20 +0530

    API assignment assert response

--- src/test/java/ListOfUsers.java
+++ src/test/java/ListOfUsers.java
@@ -1,5 +1,6 @@
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.hamcrest.Matchers;
 import org.testng.annotations.Test;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import static io.restassured.RestAssured.given;
@@ -13,28 +14,36 @@ public class ListOfUsers {
            .get("https://dummyapi.io/data/v1/user?limit=10")
    .then()
            .statusCode(200)
-           .log().body();
+           .log().body()
+           .body("data",Matchers.hasSize(10));
 }
 
 @Test
- public void shouldCreateUser() throws JsonProcessingException {
-  CreateUserRequest createUserRequest = new CreateUserRequest();
-  createUserRequest.setEmail("jafar343@gmail.com");
-  createUserRequest.setFirstName("Jafar");
-  createUserRequest.setLastName("juturu");
-
- ObjectMapper mapper = new ObjectMapper();
- //Converting the Object to JSONString
- String jsonString = mapper.writeValueAsString(createUserRequest);
- System.out.println(jsonString);
+ public void shouldCreateUser() {
+      //  throws JsonProcessingException {
+//  CreateUserRequest createUserRequest = new CreateUserRequest();
+//  createUserRequest.setEmail("jafar343@gmail.com");
+//  createUserRequest.setFirstName("Jafar");
+//  createUserRequest.setLastName("juturu");
+//
+// ObjectMapper mapper = new ObjectMapper();
+// //Converting the Object to JSONString
+// String jsonString = mapper.writeValueAsString(createUserRequest);
+// System.out.println(jsonString);
   given()
           .header("app-id","631039b2b2ee91048226aa57")
-          .body(jsonString)
+          .body("{\n" +
+                  "    \"firstName\": \"jafar\",\n" +
+                  "    \"lastName\": \"juturu\",\n" +
+                  "    \"email\": \"jafar7@gmail.com\"\n" +
+                  "}")
   .when()
           .post("https://dummyapi.io/data/v1/user/create")
   .then()
           .log().body()
-          .statusCode(200);
+          .statusCode(200)
+          .body("id", Matchers.notNullValue())
+          .body("email",Matchers.equalTo("jafar7@gmail.com"));
 }
 
 @Test
@@ -64,5 +73,21 @@ public class ListOfUsers {
   .then()
           .log().body()
           .statusCode(200);
+}
+
+@Test
+ public void shouldCreateUserWithExistingUser() {
+   given()
+         .header("app-id","631039b2b2ee91048226aa57")
+         .body("{\n" +
+                 "    \"firstName\": \"jafar\",\n" +
+                 "    \"lastName\": \"juturu\",\n" +
+                 "    \"email\": \"jafar7@gmail.com\"\n" +
+                 "}")
+   .when()
+         .post("https://dummyapi.io/data/v1/user/create")
+   .then()
+         .log().body()
+         .statusCode(200);
 }
 }
