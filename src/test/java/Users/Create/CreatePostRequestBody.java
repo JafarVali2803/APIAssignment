4aee12ce21a359bba2f94582103a6a8ab448030b
Jafar Vali Juturu <jafarj@testvagrant.com>
2022-09-12 11:32:06 +0530

    API assigment

--- src/test/java/Users/Create/CreatePostRequestBody.java
+++ src/test/java/Users/Create/CreatePostRequestBody.java
@@ -12,6 +12,6 @@ public class CreatePostRequestBody {
     private String text;
     private String image;
     private int likes;
-    private ArrayList<String> tags;
+    private String tags;
     private String owner;
 }
